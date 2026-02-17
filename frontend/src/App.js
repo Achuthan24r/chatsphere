70dd7299c4abc37d857359d96e1faa7b1e389559
Achuthan R <achuabi2419@gmail.com>
2026-02-17 21:05:31 +0530

    Fixed frontend submodule issue

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>ChatSphere 🚀</h1>
+      <p>Frontend Working Successfully</p>
+    </div>
+  );
+}
+
+export default App;
